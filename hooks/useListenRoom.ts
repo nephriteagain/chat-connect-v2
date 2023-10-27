0efb5b9522adcd684fdc8e9512d1558fd9896762
nephriteagain <oneabove232@gmail.com>
2023-10-27 12:49:17 +0800

    redirect when channel does not exist

--- hooks/useListenRoom.ts
+++ hooks/useListenRoom.ts
@@ -1,13 +1,14 @@
 import { useEffect } from "react";
 import { db } from "@/db/firebase";
-import { useParams } from "next/navigation";
+import { useParams, useRouter } from "next/navigation";
 import { onSnapshot, doc } from "firebase/firestore";
 import { useAppSelector, useAppDispatch } from "@/redux/hooks";
 import { getChannel } from "@/redux/channelSlice";
 import type { room } from "@/types";
 
 export function useListenRoom() {
-    const {channel} = useParams()
+    const { channel } = useParams()
+    const router = useRouter()
     const { channel : room } = useAppSelector(s => s.channel)
     const dispatch = useAppDispatch()
 
@@ -17,6 +18,7 @@ export function useListenRoom() {
         const unsub = onSnapshot(docRef,  (snapshot) => {
             if (!snapshot.exists()) {
                 dispatch(getChannel(null))
+                router.replace('/')
             };
             const doc = snapshot.data()            
             const data = {...doc, id: channel} as room
